bcd74c548f1b3ead880ac7788f66b89877d8dad0
arsalannury <arsiiiland@yahoo.com>
2023-05-25 23:56:50 +0430

    🎨 setup dotenv and db start

--- backend/src/setup/index.js
+++ backend/src/setup/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const favorite_route = require("../routes/favorites_route");
+
+const corsOriginOption = {
+  origin: "http://localhost:8000",
+};
+dotenv.config({ path: "./config.env" });
 
 const application = express();
 application.use(express.json());
+application.use(cors(corsOriginOption));
+application.use("/favorites", favorite_route);
 
 module.exports = application;
--- backend/src/setup/server.js
+++ backend/src/setup/server.js
@@ -1,5 +1,6 @@
 const app = require('./index');
 
 app.listen(8000, () => {
-    console.log('server start on port 8000');
+    // console.log('server start on port 8000'); 
+    console.log(process.env);
 })
