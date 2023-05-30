180ac78de451fc03ac085fcf3400bc9282ad6349
arsalannury <arsiiiland@yahoo.com>
2023-05-31 00:24:39 +0430

    📝

--- backend/src/setup/index.js
+++ backend/src/setup/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const favorite_route = require("../routes/favorites_route");
@@ -7,6 +8,18 @@ const corsOriginOption = {
   origin: "http://localhost:8000",
 };
 dotenv.config({ path: "./config.env" });
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString, { useNewUrlParser: true });
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database connected");
+});
 
 const application = express();
 application.use(express.json());
--- backend/src/setup/server.js
+++ backend/src/setup/server.js
@@ -1,6 +1,5 @@
 const app = require('./index');
 
 app.listen(8000, () => {
-    // console.log('server start on port 8000'); 
-    console.log(process.env);
+    console.log('server start on port 8000'); 
 })
